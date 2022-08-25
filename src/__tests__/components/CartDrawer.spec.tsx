f4562bf64604400798b0e760255bea7815f89c1b
Wallace Júnior <wflj1997@hotmail.com>
2022-08-25 02:34:41 -0300

    Test: CartDrawer component

---
+++ src/__tests__/components/CartDrawer.spec.tsx
@@ -0,0 +1,168 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { CartDrawer } from "~/components/CartDrawer";
+import { useCartDrawerContext } from "~/contexts/CartDrawerContext";
+import { useCartItems } from "~/hooks/useCartItems";
+import { useDispatch } from "~/redux/hooks";
+import { cartActions } from "~/redux/slices/cart";
+import { TestWrapperComponent } from "../TestWrapperComponent";
+
+jest.mock("~/contexts/CartDrawerContext");
+jest.mock("~/hooks/useCartItems");
+jest.mock("~/redux/hooks");
+
+const mockedUseCartDrawerContext = jest.mocked(useCartDrawerContext);
+const mockedUseCartItems = jest.mocked(useCartItems);
+const mockedUseDispatch = jest.mocked(useDispatch);
+
+mockedUseCartDrawerContext.mockReturnValue({} as any);
+mockedUseCartItems.mockReturnValue([]);
+mockedUseDispatch.mockReturnValue(jest.fn());
+
+describe("Header component", () => {
+  it("should render title", () => {
+    render(<CartDrawer />, { wrapper: TestWrapperComponent });
+
+    expect(screen.getByText(/Carrinho/)).toBeInTheDocument();
+  });
+
+  it("should be hidden when not open", () => {
+    mockedUseCartDrawerContext.mockReturnValueOnce({
+      isOpen: false,
+    } as any);
+
+    render(<CartDrawer />, { wrapper: TestWrapperComponent });
+
+    expect(screen.getByRole("complementary")).toHaveStyle({
+      transform: "translateX(100%)",
+    });
+  });
+
+  it("should be visible when open", () => {
+    mockedUseCartDrawerContext.mockReturnValueOnce({
+      isOpen: true,
+    } as any);
+
+    render(<CartDrawer />, { wrapper: TestWrapperComponent });
+
+    expect(screen.getByRole("complementary")).toHaveStyle({
+      transform: "translateX(0)",
+    });
+  });
+
+  it("should close drwaer on close button click", async () => {
+    const mockedClose = jest.fn();
+
+    mockedUseCartDrawerContext.mockReturnValueOnce({
+      close: mockedClose,
+    } as any);
+
+    render(<CartDrawer />, { wrapper: TestWrapperComponent });
+
+    await userEvent.click(screen.getByTitle("Fechar"));
+
+    expect(mockedClose).toHaveBeenCalled();
+  });
+
+  it("should render empty cart text when cart is empty", async () => {
+    mockedUseCartItems.mockReturnValueOnce([]);
+
+    render(<CartDrawer />, { wrapper: TestWrapperComponent });
+
+    expect(screen.getByText("Seu carrinho está vazio.")).toBeInTheDocument();
+  });
+
+  it("should not render empty cart text when cart is not empty", async () => {
+    mockedUseCartItems.mockReturnValueOnce([
+      {
+        product: {
+          id: 1,
+          name: "First Product",
+          price: 0,
+        } as any,
+        quantity: 1,
+      },
+    ]);
+
+    render(<CartDrawer />, { wrapper: TestWrapperComponent });
+
+    expect(
+      screen.queryByText("Seu carrinho está vazio."),
+    ).not.toBeInTheDocument();
+  });
+
+  it("should render all items in cart", async () => {
+    mockedUseCartItems.mockReturnValueOnce([
+      {
+        product: {
+          id: 1,
+          name: "Product 1",
+          price: 0,
+        } as any,
+        quantity: 2,
+      },
+      {
+        product: {
+          id: 2,
+          name: "Product 2",
+          price: 0,
+        } as any,
+        quantity: 1,
+      },
+    ]);
+
+    render(<CartDrawer />, { wrapper: TestWrapperComponent });
+
+    expect(screen.getByText("Product 1")).toBeInTheDocument();
+    expect(screen.getByText("Product 2")).toBeInTheDocument();
+  });
+
+  it("should render Total text", () => {
+    render(<CartDrawer />, { wrapper: TestWrapperComponent });
+
+    expect(screen.getByText("Total:")).toBeInTheDocument();
+  });
+
+  it("should render total price correctly", async () => {
+    mockedUseCartItems.mockReturnValueOnce([
+      {
+        product: {
+          id: 1,
+          price: 1000,
+        } as any,
+        quantity: 2,
+      },
+      {
+        product: {
+          id: 2,
+          price: 2000,
+        } as any,
+        quantity: 1,
+      },
+    ]);
+
+    render(<CartDrawer />, { wrapper: TestWrapperComponent });
+
+    expect(screen.getByText("R$4000")).toBeInTheDocument();
+  });
+
+  it("should clear cart and close drawer on complete purchase button click", async () => {
+    const mockedClose = jest.fn();
+    const mockedDispatch = jest.fn();
+
+    mockedUseCartDrawerContext.mockReturnValueOnce({
+      close: mockedClose,
+    } as any);
+
+    mockedUseDispatch.mockReturnValue(mockedDispatch);
+
+    render(<CartDrawer />, { wrapper: TestWrapperComponent });
+
+    await userEvent.click(
+      screen.getByRole("button", { name: "Finalizar Compra" }),
+    );
+
+    expect(mockedClose).toHaveBeenCalled();
+    expect(mockedDispatch).toHaveBeenCalledWith(cartActions.clearItems());
+  });
+});
